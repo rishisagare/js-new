5905d7a73fa8e13edb73a98677620cfbc6437e7e
rishisagare <160752476+rishisagare@users.noreply.github.com>
2024-12-21 06:42:18 +0000

    conversion and operation

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score=33
+
+//console.log(typeof score);
+
+
+//"33" => 33
+// "33abc"=>NaN
+//true =>1;false=>0
+
+let isLoggedIn=""
+
+let booleanisloggedin=Boolean(isLoggedIn)
+//console.log(booleanisloggedin);
+// 1=> true; 0 =>false
+//""=>false
+//"rishi"=>true
+
+let someNumber =33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber );
+
+
